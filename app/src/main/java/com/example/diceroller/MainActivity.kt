188795effcaf68c7498322ebe482f382da1b31b6
Sergei Shlegel <serg-69@mail.ru>
2023-02-07 00:15:12 +0300

    MainActivity.kt modifed Changed activity_main.xml

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,11 +3,11 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,12 +17,19 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val resultText:TextView = findViewById(R.id.result_text)
         val randomInt = java.util.Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
-
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
