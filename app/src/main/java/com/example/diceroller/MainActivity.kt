7032a26597ad1d9656d053a0ac5fb539a2f7b22a
Sergei Shlegel <serg-69@mail.ru>
2023-02-06 22:41:01 +0300

    MainActivity.kt added function Random

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,14 @@ class MainActivity : AppCompatActivity() {
         val rollButton:Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText:TextView = findViewById(R.id.result_text)
+        val randomInt = java.util.Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
+
     }
 }
